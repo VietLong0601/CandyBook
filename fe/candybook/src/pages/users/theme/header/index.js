957ca77ca1f59f292dc4ad1437c6df5bc78eed02
Admin <levietlongx2@gmail.com>
2024-11-01 15:34:13 +0700

    add profile

---
+++ fe/candybook/src/pages/users/theme/header/index.js
@@ -0,0 +1,6 @@
+import {memo} from "react";
+const Header = () => {
+    return <h1>Headerr</h1>;
+}
+
+export default memo(Header);  
---
+++ fe/candybook/src/router.js
@@ -0,0 +1,37 @@
+
+import { ROUTERS } from "./utils/router";
+import HomePage from "./pages/users/homepage";
+import {Routes,Route} from "react-router-dom";
+import MasterLayout from "./pages/users/theme/masterLayout";
+import Profilepage from "./pages/users/profilePage";
+
+
+const renderUserRouter=()=>{   
+    const userRouters=[
+        {
+            path: ROUTERS.USER.HOME,
+            component: <HomePage />,
+        },
+        {
+            path: ROUTERS.USER.PROFILE,
+            component: <Profilepage/>,
+        },
+    
+    ];
+    return(
+        <MasterLayout>
+        <Routes>
+            {
+                userRouters.map((item, key)=>(
+                    <Route key={key} path={item.path} element={item.component}/> 
+                ))
+            }
+        </Routes>
+        </MasterLayout>
+    )
+}
+const RouterCustom = ()=>{
+    return renderUserRouter();
+}
+
+export default RouterCustom;
